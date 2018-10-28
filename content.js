4d0d5f58ec8394942cd3a1cefc1d12d9a08ea9b9
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-10-28 20:25:09 -0300

    initial commit

---
+++ content.js
@@ -0,0 +1,20 @@
+let googleLogo = document.getElementsByTagName('img');
+googleLogo[1].src = 'https://cdn0.vox-cdn.com/thumbor/0l4Rv32qNB5YPN4s1mBDqH8EgNU=/800x0/filters:no_upscale()/cdn0.vox-cdn.com/uploads/chorus_asset/file/4019334/OGB-INSIDER-BLOGS-GoogleLogox2-Animated.0.gif';
+googleLogo[1].height = "200";
+googleLogo[1].width = "354";
+
+let divLogo = document.getElementById('hplogo');
+divLogo.style.height = "200px";
+divLogo.style.width = "354px";
+
+let searchInput = document.getElementsByClassName('sbibod');
+let searchInputB = document.getElementsByClassName('sbib_a');
+searchInput[0].style.borderRadius = "50px";
+searchInputB[0].style.borderRadius = "50px";
+
+let searchBtn = document.getElementsByName('btnK');
+let luckyBtn = document.getElementsByName('btnI');
+searchBtn[0].style.borderRadius = "50px";
+searchBtn[0].style.boxShadow = "1px 4px 5px #e0e0e0";
+luckyBtn[0].style.borderRadius = "50px";
+luckyBtn[0].style.boxShadow = "1px 4px 5px #e0e0e0";
